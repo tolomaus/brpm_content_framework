3c4098c0217b2831b13e5eaffaff13c3234fef7f
tolomaus <niek.bartholomeus@gmail.com>
2015-03-17 10:26:18 +0100

    install brpm - improved installation script

--- infrastructure/shell_scripts/install_brpm.sh
+++ infrastructure/shell_scripts/install_brpm.sh
@@ -4,21 +4,22 @@ chkconfig iptables off
 yum update
 yum install -y wget nano curl unzip
 read -p "What is the location of the RLM package?" ftp_location
-wget -O RML.zip $ftp_location # doesnt work: it still saves it under RLM_...zip
+wget -O RML.zip $ftp_location
 unzip RLM.zip
 cd BRLM/Disk1/
-read -p "Press [enter] to modify silent_install_options.txt..."
-nano silent_install_options.txt
+read -p "What is the location of the silent install file?" silent_install_file_location
 chmod +x setup.sh
-./setup.sh -i silent -DOPTIONS_FILE=silent_install_options.txt
+./setup.sh -i silent -DOPTIONS_FILE=$silent_install_file_location
 
-#TODO: make sure the $(hostname) is a publicly available url to allow the stomp stuff to work
+echo "The hostname is $(hostname)"
+read -p "Make sure the $(hostname) is a publicly available url to allow the stomp stuff to work (nano /etc/sysconfig/network), then press [enter]"
 
 echo "Stopping BRPM..."
 /etc/init.d/bmcrpm-4.6.00 stop
 
 echo "Replacing localhost to the public hostname in torquebox.yml ..."
-sed -i -e s/localhost/$(hostname)/g $BRPM_HOME/releases/$NEW_VERSION/RPM/config/torquebox.yml
+CURRENT_VERSION=$(eval "sed -n \"s=  root: $BRPM_HOME/releases/\(.*\)/RPM=\1=p\" $BRPM_HOME/server/jboss/standalone/deployments/RPM-knob.yml")
+sed -i -e s/localhost/$(hostname)/g $BRPM_HOME/releases/$CURRENT_VERSION/RPM/config/torquebox.yml
 
 echo "Restarting BRPM..."
 /etc/init.d/bmcrpm-4.6.00 start
