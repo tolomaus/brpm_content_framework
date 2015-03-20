d4c3dc8bcee3c696f791906df2984575a75eabdc
tolomaus <niek.bartholomeus@gmail.com>
2015-03-20 14:57:39 +0100

    integrations - improvements

--- infrastructure/integrations/webhook_receiver.rb
+++ infrastructure/integrations/webhook_receiver.rb
@@ -3,7 +3,7 @@ require 'rubygems'
 require 'json'
 require 'rest-client'
 require 'webrick'
-require "#{File.dirname(__FILE__)}/../bootstrap"
+require "#{File.dirname(__FILE__)}/../../bootstrap"
 
 Logger.initialize({ "log_file" => ENV["WEBHOOK_RECEIVER_LOG_FILE"] })
 
