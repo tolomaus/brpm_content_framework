f202886ad55e19c82272680a4e0a273490942344
tolomaus <niek.bartholomeus@gmail.com>
2015-04-03 16:59:27 +0200

    jira lib - changed cookie to username/password - fix

--- jira/automations/transition_issues_for_run/script.rb
+++ jira/automations/transition_issues_for_run/script.rb
@@ -21,17 +21,13 @@ def execute_script(params)
     end
   end
 
+  Logger.log "Logging in to JIRA instance #{params["SS_integration_dns"]} with username #{params["SS_integration_username"]}..."
   jira_client = Jira::Client.new(params["SS_integration_username"],
                                  decrypt_string_with_prefix(params["SS_integration_password_enc"]),
                                  params["SS_integration_dns"])
-
-  Logger.log "Logging in to JIRA instance #{params["SS_integration_dns"]} with username #{params["SS_integration_username"]}..."
-  jira_client.login()
 
   tickets.each do |ticket|
     Logger.log "Setting the status of issue #{ticket["foreign_id"]} to #{params["target_issue_status"]}"
     jira_client.set_issue_to_status(ticket["foreign_id"], params["target_issue_status"])
   end
-
-  jira_client.logout()
 end
--- jira/automations/update_release/script.rb
+++ jira/automations/update_release/script.rb
@@ -1,13 +1,11 @@
 require "jira/lib/jira_rest_api"
 
 def execute_script(params)
+  Logger.log "Logging in to JIRA instance #{params["SS_integration_dns"]} with username #{params["SS_integration_username"]}..."
   jira_client = Jira::Client.new(params["SS_integration_username"],
                                  params["SS_integration_password"] || decrypt_string_with_prefix(params["SS_integration_password_enc"]),
                                  params["SS_integration_dns"])
 
-  Logger.log "Logging in to JIRA instance #{params["SS_integration_dns"]} with username #{params["SS_integration_username"]}..."
-  jira_client.login()
   Logger.log "Updating option for release from #{params["old_release_name"]} to #{params["new_release_name"]} in the JIRA dropdown custom field with id #{params["jira_release_field_id"]}..."
   jira_client.update_option_for_dropdown_custom_field(params["jira_release_field_id"], params["old_release_name"], params["new_release_name"])
-  jira_client.logout()
 end
