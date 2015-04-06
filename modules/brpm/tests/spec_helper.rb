01475259c128fc3b4d7cc4808eb98e1174264308
tolomaus <niek.bartholomeus@gmail.com>
2015-04-06 16:23:54 +0200

    Fixed missing log file dir for travis

--- modules/brpm/tests/spec_helper.rb
+++ modules/brpm/tests/spec_helper.rb
@@ -1,5 +1,7 @@
 require "#{File.dirname(__FILE__)}/../../framework/bootstrap"
+require 'fileutils'
 
+Dir.mkdir_p "/tmp/brpm_content/"
 Logger.initialize({ "log_file" => "/tmp/brpm_content/tests.log" }) # TODO clean up
 
 require "brpm/lib/brpm_rest_api"
@@ -10,7 +12,6 @@ def get_default_params
   params['SS_base_url'] = 'http://brpm-content.pulsar-it.be:8088/brpm' # TODO "http://#{ENV["BRPM_HOST"]}:#{ENV["BRPM_PORT"]}/brpm"
   params['SS_api_token'] = ENV["BRPM_API_TOKEN"]
 
-  require 'fileutils'
   dir = "/tmp/brpm_content/step_1"
   params['SS_output_dir'] = dir # TODO clean up
 
