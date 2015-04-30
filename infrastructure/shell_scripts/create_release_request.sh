685a5bc0955ec5bd0d7c40801d540da85a4bade4
tolomaus <niek.bartholomeus@gmail.com>
2015-04-30 17:19:40 +0200

    check for empty var in shell script create release request

--- infrastructure/shell_scripts/create_release_request.sh
+++ infrastructure/shell_scripts/create_release_request.sh
@@ -8,7 +8,7 @@ params = {}
 params["application_name"] = "$APPLICATION_NAME"
 params["application_version"] = "$APPLICATION_VERSION"
 params["release_request_template_name"] = "$RELEASE_REQUEST_TEMPLATE_NAME"
-if [ -n ${RELEASE_PLAN_TEMPLATE_NAME+x} ]; then params["release_plan_template_name"] = "$RELEASE_PLAN_TEMPLATE_NAME"; fi
+params["release_plan_template_name"] = "$RELEASE_PLAN_TEMPLATE_NAME" unless "$RELEASE_PLAN_TEMPLATE_NAME".empty?
 
 params["brpm_url"] = "http://$BRPM_HOST:$BRPM_PORT/brpm"
 params["brpm_api_token"] = "$BRPM_TOKEN"
