9cd9b73946fd134e1388b90e8170c9165ff586e5
tolomaus <niek.bartholomeus@gmail.com>
2015-05-01 15:48:06 +0200

    Reworked docker_brpm structure

--- modules/brpm/automations/select_component_version.rb
+++ modules/brpm/automations/select_component_version.rb
@@ -1,10 +1,9 @@
 params = BrpmAuto.params
-request_params = BrpmAuto.request_params
 
-component_versions = request_params["component_versions"] || {}
+component_versions = BrpmAuto.request_params["component_versions"] || {}
 
 component_versions[params["component"]] = params["component_version"]
 
 BrpmAuto.log "Adding component version '#{params["component"]}' '#{params["component_version"]}' to the request_params..."
-request_params["component_versions"] = component_versions
+BrpmAuto.request_params["component_versions"] = component_versions
 
