a5836728fa6c8949fe1afd6a9854efd46e45d3ef
tolomaus <niek.bartholomeus@gmail.com>
2015-05-04 20:43:06 +0200

    Create release request - allow passing component versions - improve auto_create logic

--- modules/brpm/automations/select_component_version.rb
+++ modules/brpm/automations/select_component_version.rb
@@ -1,9 +1,12 @@
-params = BrpmAuto.params
+if BrpmAuto.request_params["auto_created"]
+  BrpmAuto.log "The request was created in an automated way, not overriding the request params from the manual input step."
+else
+  params = BrpmAuto.params
+  component_versions = BrpmAuto.request_params["component_versions"] || {}
 
-component_versions = BrpmAuto.request_params["component_versions"] || {}
+  BrpmAuto.log "Adding component version '#{params["component"]}' '#{params["component_version"]}' to the request_params..."
+  component_versions[params["component"]] = params["component_version"]
 
-component_versions[params["component"]] = params["component_version"] unless BrpmAuto.request_params["auto_created"]
-
-BrpmAuto.log "Adding component version '#{params["component"]}' '#{params["component_version"]}' to the request_params..."
-BrpmAuto.request_params["component_versions"] = component_versions
+  BrpmAuto.request_params["component_versions"] = component_versions
+end
 
