1a93634c44e4e0748af03663ea4e9c2ca0bc5744
tolomaus <niek.bartholomeus@gmail.com>
2015-05-04 12:45:24 +0200

    Fix dockerized brpm launch test run in jenkins

--- modules/docker_brpm/automations/execute_test_run_in_jenkins.rb
+++ modules/docker_brpm/automations/execute_test_run_in_jenkins.rb
@@ -12,7 +12,7 @@ build_number = result["response"]["nextBuildNumber"]
 
 # TODO: find out why passing the parameters as a json structure in the body doesn't work
 job_params = []
-job_params << { :name => "DOCKER_HOST", :value => get_docker_host_name }
+job_params << { :name => "BRPM_HOST", :value => get_docker_host_name }
 job_params << { :name => "BRPM_PORT", :value => params["port"] }
 job_params << { :name => "BRPM_API_TOKEN", :value => params["api_token"] }
 
@@ -21,7 +21,7 @@ options = {}
 options["username"] = jenkins_username
 options["password"] = jenkins_password
 
-jenkins_test_rest_api_url = "#{jenkins_host}/job/#{jenkins_job}/buildWithParameters?DOCKER_HOST=#{get_docker_host_name}&BRPM_PORT=#{params["port"]}&BRPM_API_TOKEN=#{params["api_token"]}"
+jenkins_test_rest_api_url = "#{jenkins_host}/job/#{jenkins_job}/buildWithParameters?BRPM_HOST=#{get_docker_host_name}&BRPM_PORT=#{params["port"]}&BRPM_API_TOKEN=#{params["api_token"]}"
 
 BrpmAuto.log "Triggering jenkins to execute the tests ..."
 Rest.post jenkins_test_rest_api_url, { :parameter => job_params }, options
