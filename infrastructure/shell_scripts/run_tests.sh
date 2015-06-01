25a202d88d5b9f9facbf11cdd21ff2cc1a6da4ee
tolomaus <niek.bartholomeus@gmail.com>
2015-06-01 17:48:12 +0200

    run_tests.sh: take into account failed tests of other modules than the last one

--- infrastructure/shell_scripts/run_tests.sh
+++ infrastructure/shell_scripts/run_tests.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
+failed_tests = false
+
 bundle exec rspec ./modules/brpm/tests --format documentation
+rc=$?; if [[ $rc != 0 ]]; then failed_tests = true; fi
+
 bundle exec rspec ./modules/bladelogic/tests --format documentation
+rc=$?; if [[ $rc != 0 ]]; then failed_tests = true; fi
+
 bundle exec rspec ./modules/jira/tests --format documentation
+rc=$?; if [[ $rc != 0 ]]; then failed_tests = true; fi
+
+if [ $failed_tests = true ] ; then
+ exit -1;
+fi
