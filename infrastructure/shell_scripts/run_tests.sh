72d951dbc713f4b58314a7fc303ad2c0756bc47c
tolomaus <niek.bartholomeus@gmail.com>
2015-06-12 16:09:33 +0200

    Fix bug with run_tests.sh not alerting when errors happened

--- infrastructure/shell_scripts/run_tests.sh
+++ infrastructure/shell_scripts/run_tests.sh
@@ -2,17 +2,18 @@
 failed_tests = false
 
 bundle exec rspec ./modules/framework/tests --format documentation
-rc=$?; if [[ $rc != 0 ]]; then failed_tests = true; fi
+rc=$?; if [ $rc != 0 ]; then failed_tests=true; fi
 
 bundle exec rspec ./modules/brpm/tests --format documentation
-rc=$?; if [[ $rc != 0 ]]; then failed_tests = true; fi
+rc=$?; if [ $rc != 0 ]; then failed_tests=true; fi
 
 bundle exec rspec ./modules/jira/tests --format documentation
-rc=$?; if [[ $rc != 0 ]]; then failed_tests = true; fi
+rc=$?; if [ $rc != 0 ]; then failed_tests=true; fi
 
 bundle exec rspec ./modules/bladelogic/tests --format documentation
-rc=$?; if [[ $rc != 0 ]]; then failed_tests = true; fi
+rc=$?; if [ $rc != 0 ]; then failed_tests = true; fi
 
 if [ "$failed_tests" = true ] ; then
- exit -1;
+  echo "Failed tests were found, exiting with -1."
+  exit -1;
 fi
