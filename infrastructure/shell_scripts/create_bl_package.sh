a9c3923ce25fbb2181f8a1243699df44d9f5490e
tolomaus <niek.bartholomeus@gmail.com>
2015-06-24 14:26:27 +0200

    Added home dir check in shell wrapper schrips

--- infrastructure/shell_scripts/create_bl_package.sh
+++ infrastructure/shell_scripts/create_bl_package.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-. /opt/bmc/RLM/bin/setenv.sh
+
+# ASSUMPTION: this script should be executed from the root direcory of the framework
+
+if [ -z "$BRPM_HOME" ]; then
+    echo "BRPM_HOME is not set (e.g. /opt/bmc/RLM). Aborting the installation."
+    exit 1
+fi
+
+. $BRPM_HOME/bin/setenv.sh
 
 jruby <<-EORUBY
 require "modules/framework/brpm_script_executor"
