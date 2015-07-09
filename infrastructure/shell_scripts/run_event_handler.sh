25ab7e0a5d08ce5a5cc81558d9e17fdda14ed023
tolomaus <niek.bartholomeus@gmail.com>
2015-07-09 11:54:48 +0200

    Added executable ruby scripts for event handler and webhook receiver to the gem

--- infrastructure/shell_scripts/run_event_handler.sh
+++ infrastructure/shell_scripts/run_event_handler.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-if [ -z "$BRPM_HOME" ]; then
-    echo "BRPM_HOME is not set (e.g. /opt/bmc/RLM). Aborting the script."
-    exit 1
-fi
-
 # mandatory settings
 export EVENT_HANDLER_BRPM_HOST=localhost
 
@@ -12,7 +6,7 @@ export EVENT_HANDLER_MESSAGING_PORT=5445
 export EVENT_HANDLER_MESSAGING_USERNAME=msguser
 export EVENT_HANDLER_MESSAGING_PASSWORD=????
 export EVENT_HANDLER_LOG_FILE=/tmp/event_handler.log
-export EVENT_HANDLER_PROCESS_EVENT_SCRIPT=customers/demo/integrations/brpm/process_event_handler_event.rb
+export EVENT_HANDLER_PROCESS_EVENT_SCRIPT=integrations/brpm/process_event_handler_event.rb
 
 # custom settings
 export EVENT_HANDLER_BRPM_PORT=8088
@@ -23,8 +17,4 @@ export EVENT_HANDLER_JIRA_USERNAME=????
 export EVENT_HANDLER_JIRA_PASSWORD=????
 export EVENT_HANDLER_JIRA_RELEASE_FIELD_ID=????
 
-VERSION=$(eval "sed -n \"s=  root: $BRPM_HOME/releases/\(.*\)/RPM=\1=p\" $BRPM_HOME/server/jboss/standalone/deployments/RPM-knob.yml")
-
-. $BRPM_HOME/bin/setenv.sh
-
-jruby $BRPM_HOME/releases/$VERSION/RPM/lib/script_support/git_repos/brpm_content/infrastructure/integrations/event_handler.rb
+event_handler
--- infrastructure/shell_scripts/run_webhook_receiver.sh
+++ infrastructure/shell_scripts/run_webhook_receiver.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
-
-if [ -z "$BRPM_HOME" ]; then
-    echo "BRPM_HOME is not set (e.g. /opt/bmc/RLM). Aborting the script."
-    exit 1
-fi
-
 # mandatory settings
 export WEBHOOK_RECEIVER_PORT=8089
 export WEBHOOK_RECEIVER_MOUNT_POINT=webhooks
 export WEBHOOK_RECEIVER_LOG_FILE=/tmp/webhook_receiver.log
 export WEBHOOK_RECEIVER_INTEGRATION_ID=????
-export WEBHOOK_RECEIVER_PROCESS_EVENT_SCRIPT=customers/demo/integrations/jira/process_webhook_event.rb
+export WEBHOOK_RECEIVER_PROCESS_EVENT_SCRIPT=integrations/jira/process_webhook_event.rb
 
 # custom settings
 export WEBHOOK_RECEIVER_BRPM_HOST=localhost
@@ -18,8 +12,4 @@ export WEBHOOK_RECEIVER_BRPM_PORT=8088
 export WEBHOOK_RECEIVER_BRPM_TOKEN=????
 export WEBHOOK_RECEIVER_JIRA_RELEASE_FIELD_ID=????
 
-VERSION=$(eval "sed -n \"s=  root: $BRPM_HOME/releases/\(.*\)/RPM=\1=p\" $BRPM_HOME/server/jboss/standalone/deployments/RPM-knob.yml")
-
-. $BRPM_HOME/bin/setenv.sh
-
-jruby -J-Djava.library.path= $BRPM_HOME/releases/$VERSION/RPM/lib/script_support/git_repos/brpm_content/infrastructure/integrations/webhook_receiver.rb
+jruby -J-Djava.library.path= webhook_receiver.rb
