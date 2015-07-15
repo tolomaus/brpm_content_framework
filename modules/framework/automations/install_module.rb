dec89eb57e5324e4ccba58e1ca793254aaaf97ab
tolomaus <niek.bartholomeus@gmail.com>
2015-07-15 21:15:24 +0200

    Added an automation script for installing modules - fix bugs

--- modules/framework/automations/install_module.rb
+++ modules/framework/automations/install_module.rb
@@ -1,4 +1,13 @@
 BrpmAuto.params["name"]
 BrpmAuto.params["version"]
 
-BrpmAuto.execute_shell("export GEM_HOME=${BRPM_CONTENT_HOME:-$BRPM_HOME/modules} && gem install #{BrpmAuto.params["name"]} #{BrpmAuto.params["version"] ? " -v #{BrpmAuto.params["version"]}" : ""}")
+version_flag = ""
+if BrpmAuto.params["version"] and ! BrpmAuto.params["version"].empty?
+  version_flag = " -v #{BrpmAuto.params["version"]}"
+end
+
+result = BrpmAuto.execute_shell("export GEM_HOME=${BRPM_CONTENT_HOME:-$BRPM_HOME/modules} && gem install #{BrpmAuto.params["name"]} #{version_flag}")
+BrpmAuto.log result["stdout"] if result["stdout"] and ! result["stdout"].empty?
+unless result["status"] == 0
+  raise result["stderr"]
+end
