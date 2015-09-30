6166b2b8689a1c73d2e2c97ae5270f8f5c2dca66
tolomaus <niek.bartholomeus@gmail.com>
2015-09-30 11:33:15 +0200

    brpm_script_executor in separate process - add error handling Docker - fix link to result file

--- lib/brpm_script_executor.rb
+++ lib/brpm_script_executor.rb
@@ -15,112 +15,120 @@ class BrpmScriptExecutor
     end
 
     def execute_automation_script_in_separate_process_internal(modul, name, params, automation_type, parent_id = nil, offset = nil, max_records = nil)
-      BrpmAuto.setup(params)
-      BrpmAuto.log ""
-      BrpmAuto.log "Executing #{automation_type} '#{name}' from module '#{modul}' in a separate process..."
+      begin
+        BrpmAuto.setup(params)
+        BrpmAuto.log ""
+        BrpmAuto.log "Executing #{automation_type} '#{name}' from module '#{modul}' in a separate process..."
 
-      BrpmAuto.log "Finding the module's version..."
-      case automation_type
-      when "automation"
-        module_version = params["module_version"] || get_latest_installed_version(modul)
-      when "resource_automation"
-        module_version = get_latest_installed_version(modul) #TODO: get the module version of the calling script
-      else
-        raise "Automation type #{automation_type} is not supported."
-      end
+        BrpmAuto.log "Finding the module's version..."
+        case automation_type
+        when "automation"
+          module_version = params["module_version"] || get_latest_installed_version(modul)
+        when "resource_automation"
+          module_version = get_latest_installed_version(modul) #TODO: get the module version of the calling script
+        else
+          raise "Automation type #{automation_type} is not supported."
+        end
 
-      case BrpmAuto.params["execute_automation_scripts_in_docker"]
-      when "always"
-        use_docker = true
-      when "if_docker_image_exists"
-        BrpmAuto.log "Checking if a docker image exists for bmcrlm/#{modul}:#{module_version}..."
-        output = `docker history -q bmcrlm/#{modul}:#{module_version} 2>&1 >/dev/null`
-        if output.empty?
+        case BrpmAuto.params["execute_automation_scripts_in_docker"]
+        when "always"
           use_docker = true
+        when "if_docker_image_exists"
+          BrpmAuto.log "Checking if a docker image exists for bmcrlm/#{modul}:#{module_version}..."
+          output = `docker history -q bmcrlm/#{modul}:#{module_version} 2>&1 >/dev/null`
+          if output.empty?
+            use_docker = true
+          else
+            BrpmAuto.log "The image doesn't exist locally, checking if we can pull it from the Docker Hub..."
+            output = `docker pull bmcrlm/#{modul}:#{module_version}`
+            use_docker = (output =~ /Image is up to date for/)
+          end
         else
-          BrpmAuto.log "The image doesn't exist locally, checking if we can pull it from the Docker Hub..."
-          output = `docker pull bmcrlm/#{modul}:#{module_version}`
-          use_docker = (output =~ /Image is up to date for/)
+          use_docker = false
         end
-      else
-        use_docker = false
-      end
-      BrpmAuto.log "The automation script will be executed in a docker container." if	use_docker
+        BrpmAuto.log "The automation script will be executed in a docker container." if	use_docker
 
-      working_path = File.expand_path(BrpmAuto.params.output_dir)
-      params_file = "params_#{params["SS_run_key"] || params["run_key"] || "000"}.yml"
-      params_path = "#{working_path}/#{params_file}"
-      automation_results_path = params["SS_automation_results_dir"] || working_path
+        working_path = File.expand_path(BrpmAuto.params.output_dir)
+        params_file = "params_#{params["SS_run_key"] || params["run_key"] || "000"}.yml"
+        params_path = "#{working_path}/#{params_file}"
+        automation_results_path = params["SS_automation_results_dir"] || working_path
 
-      if use_docker
-        params["SS_output_dir"] = "/workdir"
-        params["SS_output_file"].sub!(working_path, "/workdir") if params["SS_output_file"]
-        params["SS_automation_results_dir"] = "/automation_results"
+        if use_docker
+          params["SS_output_dir"] = "/workdir"
+          params["SS_output_file"].sub!(working_path, "/workdir") if params["SS_output_file"]
+          params["SS_automation_results_dir"] = "/automation_results"
 
-        params["log_file"].sub!(working_path, "/workdir") if params["log_file"]
-      end
+          params["log_file"].sub!(working_path, "/workdir") if params["log_file"]
+        end
 
-      BrpmAuto.log "Temporarily storing the params to #{params_path}..."
-      File.open(params_path, "w") do |file|
-        file.puts(params.to_yaml)
-      end
+        BrpmAuto.log "Temporarily storing the params to #{params_path}..."
+        File.open(params_path, "w") do |file|
+          file.puts(params.to_yaml)
+        end
 
-      if use_docker
-        BrpmAuto.log "Executing the script in a docker container..."
-        command = "docker run -v #{working_path}:/workdir -v #{automation_results_path}:/automation_results --rm bmcrlm/#{modul}:#{module_version} /docker_execute_automation \"#{name}\" \"/workdir/#{params_file}\" \"#{automation_type}\""
+        if use_docker
+          BrpmAuto.log "Executing the script in a docker container..."
+          command = "docker run -v #{working_path}:/workdir -v #{automation_results_path}:/automation_results --rm bmcrlm/#{modul}:#{module_version} /docker_execute_automation \"#{name}\" \"/workdir/#{params_file}\" \"#{automation_type}\""
+          if automation_type == "resource_automation"
+            command += " \"#{parent_id}\"" if parent_id
+            command += " \"#{offset}\"" if offset
+            command += " \"#{max_records}\"" if max_records
+          end
+          BrpmAuto.log command
+          return_value = system(command)
+        else
+          env_vars = {}
+          env_vars["GEM_HOME"] = ENV["BRPM_CONTENT_HOME"] || "#{ENV["BRPM_HOME"]}/modules"
+
+          module_path = get_module_gem_path(modul, module_version)
+
+          if File.exists?(module_path)
+            BrpmAuto.log "Found module #{modul} #{module_version || ""} in path #{module_path}."
+          else
+            raise Gem::GemNotFoundException, "Module #{modul} version #{module_version} is not installed. Expected it on path #{module_path}."
+          end
+
+          gemfile_path = "#{module_path}/Gemfile"
+          if File.exists?(gemfile_path)
+            BrpmAuto.log "Using Gemfile #{gemfile_path}."
+            env_vars["BUNDLE_GEMFILE"] = gemfile_path
+            require_bundler = "require 'bundler/setup';"
+          else
+            BrpmAuto.log("This module doesn't have a Gemfile.")
+            require_bundler = ""
+          end
+
+          BrpmAuto.log "Executing the script in a separate process..."
+          return_value = Bundler.clean_system(env_vars, "ruby", "-e", "#{require_bundler}require 'brpm_script_executor'; BrpmScriptExecutor.execute_automation_script_from_other_process(\"#{modul}\", \"#{name}\", \"#{params_path}\", \"#{automation_type}\", \"#{parent_id}\", \"#{offset}\", \"#{max_records}\")")
+        end
+
+        FileUtils.rm(params_path) if File.exists?(params_path)
+        if return_value.nil?
+          message = "The process that executed the automation script returned with 'Command execution failed'."
+          BrpmAuto.log_error message
+          raise message
+        elsif return_value == false
+          message = "The process that executed the automation script returned with non-zero exit code: #{$?.exitstatus}"
+          BrpmAuto.log_error message
+          raise message
+        end
+
         if automation_type == "resource_automation"
-          command += " \"#{parent_id}\"" if parent_id
-          command += " \"#{offset}\"" if offset
-          command += " \"#{max_records}\"" if max_records
-        end
-        BrpmAuto.log command
-        return_value = system(command)
-      else
-        env_vars = {}
-        env_vars["GEM_HOME"] = ENV["BRPM_CONTENT_HOME"] || "#{ENV["BRPM_HOME"]}/modules"
+          result_path = params_path.sub!("params", "result")
+          BrpmAuto.log "Loading the result from #{result_path} and cleaning it up..."
+          result = YAML.load_file(result_path)
+          FileUtils.rm result_path
 
-        module_path = get_module_gem_path(modul, module_version)
-
-        if File.exists?(module_path)
-          BrpmAuto.log "Found module #{modul} #{module_version || ""} in path #{module_path}."
+          result
         else
-          raise Gem::GemNotFoundException, "Module #{modul} version #{module_version} is not installed. Expected it on path #{module_path}."
+          return_value
         end
 
-        gemfile_path = "#{module_path}/Gemfile"
-        if File.exists?(gemfile_path)
-          BrpmAuto.log "Using Gemfile #{gemfile_path}."
-          env_vars["BUNDLE_GEMFILE"] = gemfile_path
-          require_bundler = "require 'bundler/setup';"
-        else
-          BrpmAuto.log("This module doesn't have a Gemfile.")
-          require_bundler = ""
-        end
+      rescue Exception => e
+        BrpmAuto.log_error "#{e}"
+        BrpmAuto.log "\n\t" + e.backtrace.join("\n\t")
 
-        BrpmAuto.log "Executing the script in a separate process..."
-        return_value = Bundler.clean_system(env_vars, "ruby", "-e", "#{require_bundler}require 'brpm_script_executor'; BrpmScriptExecutor.execute_automation_script_from_other_process(\"#{modul}\", \"#{name}\", \"#{params_path}\", \"#{automation_type}\", \"#{parent_id}\", \"#{offset}\", \"#{max_records}\")")
-      end
-
-      FileUtils.rm(params_path) if File.exists?(params_path)
-      if return_value.nil?
-        message = "The process that executed the automation script returned with 'Command execution failed'."
-        BrpmAuto.log_error message
-        raise message
-      elsif return_value == false
-        message = "The process that executed the automation script returned with non-zero exit code: #{$?.exitstatus}"
-        BrpmAuto.log_error message
-        raise message
-      end
-
-      if automation_type == "resource_automation"
-        result_file = params_file.sub!("params", "result")
-        BrpmAuto.log "Loading the result from #{result_file} and cleaning it up..."
-        result = YAML.load_file(result_file)
-        FileUtils.rm result_file
-
-        result
-      else
-        return_value
+        raise e
       end
     end
 
