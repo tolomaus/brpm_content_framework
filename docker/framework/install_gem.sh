30ef9990e4381c78348be9769b7db1cf62fdad68
tolomaus <niek.bartholomeus@gmail.com>
2015-10-04 21:54:42 +0200

    Create a build_docker_image script that can be used on the BRPM host - --ignore-dependencies still not working

--- docker/framework/install_gem.sh
+++ docker/framework/install_gem.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-gem install $MODULE -v $VERSION --ignore-dependencies
+gem install $MODULE -v $VERSION
 cd $GEM_HOME/gems/$MODULE-$VERSION
 bundle install
 rm -rf $GEM_HOME/cache
