b8a9106626a9b64a99dcf962c0ac6ba86d86faeb
tolomaus <niek.bartholomeus@gmail.com>
2015-10-12 14:57:56 +0200

    install_gem - exit 1 when one of the commands fail

--- docker/framework/install_gem.sh
+++ docker/framework/install_gem.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-gem install $MODULE -v $VERSION
+gem install $MODULE -v $VERSION || { echo 'gem install failed' ; exit 1; }
+
 cd $GEM_HOME/gems/$MODULE-$VERSION
-bundle install
+bundle install || { echo 'bundle install failed' ; exit 1; }
+
 rm -rf $GEM_HOME/cache
