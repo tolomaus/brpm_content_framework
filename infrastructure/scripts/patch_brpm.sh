c46225adbff5ac5e15083950f40b64459cfcfe6e
tolomaus <niek.bartholomeus@gmail.com>
2015-11-23 11:32:33 +0100

    Upgrade patch_brpm.sh script to BRPM 4.8

--- infrastructure/scripts/patch_brpm.sh
+++ infrastructure/scripts/patch_brpm.sh
@@ -37,7 +37,7 @@ $(dirname $0)/backup_database.sh
 echo ""
 
 echo "Stopping BRPM..."
-/etc/init.d/bmcrpm-4.6.00 stop
+/etc/init.d/bmcrpm-4.8.00 stop
 
 OLD_VERSION=$(eval "sed -n \"s=  root: $BRPM_HOME/releases/\(.*\)/RPM=\1=p\" $BRPM_HOME/server/jboss/standalone/deployments/RPM-knob.yml")
 
@@ -56,10 +56,6 @@ echo "Copying over the config files from $BRPM_HOME/releases/$OLD_VERSION/RPM/co
 /bin/cp $BRPM_HOME/releases/$OLD_VERSION/RPM/config/wicked_pdf_config.rb $BRPM_HOME/releases/$NEW_VERSION/RPM/config
 /bin/cp $BRPM_HOME/releases/$OLD_VERSION/RPM/config/carrierwave_settings.rb $BRPM_HOME/releases/$NEW_VERSION/RPM/config
 
-if [ -d $BRPM_HOME/releases/$OLD_VERSION/RPM/lib/script_support/git_repos ]; then
-  /bin/cp -R $BRPM_HOME/releases/$OLD_VERSION/RPM/lib/script_support/git_repos $BRPM_HOME/releases/$NEW_VERSION/RPM/lib/script_support
-fi
-
 echo "Replacing the version number in RPM-knob.yml ..."
 sed -i -e s/$OLD_VERSION/$NEW_VERSION/g $BRPM_HOME/server/jboss/standalone/deployments/RPM-knob.yml
 
@@ -71,8 +67,8 @@ sed -i -e s/$DEFAULT_HOSTNAME/$PREVIOUS_HOSTNAME/g $BRPM_HOME/releases/$NEW_VERS
 . $BRPM_HOME/bin/setenv.sh
 cd $BRPM_HOME/releases/$NEW_VERSION/RPM/config
 
-echo "Updating gems..."
-jruby -S bundle install
+#echo "Updating gems..."
+#jruby -S bundle install
 
 echo "Migrating the database..."
 jruby -S rake --verbose db:migrate RAILS_ENV=production
@@ -81,7 +77,7 @@ echo "Patch $NEW_VERSION was applied successfully."
 echo "Note that any customizations that may have been done to the torquebox.yml file must be redone manually: $BRPM_HOME/releases/$NEW_VERSION/RPM/config/torquebox.yml"
 
 echo "Restarting BRPM..."
-/etc/init.d/bmcrpm-4.6.00 start
+/etc/init.d/bmcrpm-4.8.00 start
 
 echo "Done."
 
