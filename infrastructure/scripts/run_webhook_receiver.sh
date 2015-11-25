19db864ea1523c051ec3e7e153b13fca632e7ed5
tolomaus <niek.bartholomeus@gmail.com>
2015-11-25 11:31:11 +0100

    Simplified the run_webhook_receiver.sh script

--- infrastructure/scripts/run_webhook_receiver.sh
+++ infrastructure/scripts/run_webhook_receiver.sh
@@ -3,13 +3,11 @@
 export WEBHOOK_RECEIVER_PORT=8089
 export WEBHOOK_RECEIVER_MOUNT_POINT=webhooks
 export WEBHOOK_RECEIVER_LOG_FILE=/tmp/webhook_receiver.log
-export WEBHOOK_RECEIVER_INTEGRATION_ID=???
-export WEBHOOK_RECEIVER_PROCESS_EVENT_SCRIPT=integrations/jira/process_webhook_event.rb
+export WEBHOOK_RECEIVER_PROCESS_EVENT_SCRIPT=integrations/xxx/process_webhook_event.rb
 
 # custom settings
 export WEBHOOK_RECEIVER_BRPM_HOST=localhost
 export WEBHOOK_RECEIVER_BRPM_PORT=8088
 export WEBHOOK_RECEIVER_BRPM_TOKEN=???
-export WEBHOOK_RECEIVER_JIRA_RELEASE_FIELD_ID=???
 
 webhook_receiver
